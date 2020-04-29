4bcf307cf4f6adb5cbe8a8e599a705dff99f1b8e
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-04-29 14:54:40 +0200

    Add changes in Entreprise view

--- resources/views/entreprises/show.blade.php
+++ resources/views/entreprises/show.blade.php
@@ -7,12 +7,27 @@
     <h1>Affichage d'une catégorie</h1>
 
     <a href="{{ route('entreprises.index') }}">Retour a l'accueil</a>
-
+    <br>
     @if(!is_null($entreprises))
         {{ $entreprises->nom }}
+        <br>
+        {{$entreprises->adresse}}
+        <br>
+        {{$entreprises->telephone}}
+        <br>
+        {{$entreprises->mail}}
     @else
         <p>l'entreprise n'existe pas</p>
     @endif
 
+    <br><br>
+    
+    <a href="/" title="Modification">Modifer les informations liées à l'entreprise</a>
+    <br>
+    
+    <a href="/" title="Supprimer l'entreprise">Supprimer l'entreprise</a>
+    <br>
+    <a href="{{ route('contact.create') }}" title="Ajouter un contact">Ajouter un contact</a>
+    <br>
 </body>
 </html>
