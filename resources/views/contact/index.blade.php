63001c91023037777c0a68e997bb1d5afa12235f
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-04-30 13:51:52 +0200

    Update inex.blade.php

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -5,14 +5,15 @@
 </head>
 <body>
     <h1>Affichage des contacts</h1>
-
-    <a href="{{ route('contact.create') }}" title="Ajouter un contact">Ajouter un contact</a>
-
     <br>
     @foreach($contact as $one)
     <ul>
     <!--Il faut trouver comment afficher le nom de l'entreprise-->
-        <li>{{ $one->entreprise }}, {{ $one->nom }} {{ $one->prenom }}</li>
+        <li>{{ $one->entreprise }}</li>
+        <li>{{ $one->nom }} {{ $one->prenom }}</li>
+        <li>{{ $one->poste }}</li>
+        <li>{{ $one->mail }}</li>
+        <li>{{ $one->numero }}</li>
     </ul>
     @endforeach
     <br><br>
