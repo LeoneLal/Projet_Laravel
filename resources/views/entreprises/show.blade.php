683422c08e18ab26546a38c0ef5825cf0e70a8da
AurianeLab <34133072+AurianeLab@users.noreply.github.com>
2020-06-05 16:18:16 +0200

    Update show.blade.php

--- resources/views/entreprises/show.blade.php
+++ resources/views/entreprises/show.blade.php
@@ -5,32 +5,27 @@
 <html>
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-	<title>Affichage d'une catégorie</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <title>Affichage d'une catégorie</title>
 </head>
 <body>
     <h1>Affichage d'une catégorie</h1>
 
-    <p>
     @if(!is_null($entreprise))
-        {{$entreprise->nom}}
-        <br>
-        {{$entreprise->adresse}}
-        <br>
-        {{$entreprise->telephone}}
-        <br>
-        {{$entreprise->mail}}
+        <p>{{$entreprise->nom}}</p>
+        <p>{{$entreprise->adresse}}</p>
+        <p>{{$entreprise->telephone}}</p>
+        <p>{{$entreprise->mail}}</p>
     @else
-        <p>l'entreprise n'existe pas</p>
+        <p>L'entreprise n'existe pas</p>
     @endif
-    </p>    
     <a href="{{ route('entreprises.edit', $entreprise->id) }}" title="Modification"><button type="button" class="btn btn-info">Modifier l'entreprise</button></a>
     <a href="{{ route('entreprises.delete', $entreprise->id) }}" title="Supprimer l'entreprise"><button type="button" class="btn btn-danger">Supprimer l'entreprise</button></a>
     <a href="{{ route('contact.create', $entreprise->id) }}" title="Ajouter un contact"><button type="button" class="btn btn-success">Ajouter un contact</button></a>
     <hr>
-    <br>
     <h2>Affichage des contacts</h2>
     <!-- Afficher les différents contact -->
-    <p>Les contacts liés à l'entreprise apparaitrons ici !</p>
+    <p>Les contacts liés à l'entreprise apparaitront ici !</p>
 
     @if(!is_null($entreprise->contact))
         @foreach($entreprise->contact as $contact)
